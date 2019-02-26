76d0b7bbb2bba181d54219e8ef412840c6394b40
Brett Wilcox <wilcox.brett@gmail.com>
2019-02-26 13:31:50 -0700

    Initial file/folder setup and push.

---
+++ index.js
@@ -0,0 +1,27 @@
+let express = require('express')
+let bp = require('body-parser')
+let server = express()
+let port = 3000
+
+
+//initialize connection to database
+require('./server-assets/database/gearhost-config')
+
+//middlewear
+server.use(bp.json())
+server.use(bp.urlencoded({ extended: true }))
+
+
+//routes
+
+
+//default
+server.use('*', (req, res, next) => {
+  res.status(404).send('No Routes Matching That Request')
+})
+
+//start server
+server.listen(port, () => {
+  console.log('Server Running on Port:', port)
+})
+
